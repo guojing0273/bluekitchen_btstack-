1f60f599beb67c5c08869d39176a2b27c36d9263
Matthias Ringwald <matthias@ringwald.ch>
2018-08-13 10:49:33 +0200

    example/le_streamer_client: use 0x30-0x40 as adv interval

--- example/le_streamer_client.c
+++ example/le_streamer_client.c
@@ -470,7 +470,7 @@ int btstack_main(int argc, const char * argv[]){
 #ifdef ENABLE_ADVERTISEMENTS
     // setup advertisements
     uint16_t adv_int_min = 0x0030;
-    uint16_t adv_int_max = 0x0030;
+    uint16_t adv_int_max = 0x0040;
     uint8_t adv_type = 0;
     bd_addr_t null_addr;
     memset(null_addr, 0, 6);
