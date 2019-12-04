f90e6b01ba0e4a5d8893fdaa77c40af3a08fbd93
TREFOU Felix <ftrefou@onewave.fr>
2019-12-04 12:27:24 +0100

    stm32-wb55: Updated ST libraries, WPAN and HAL, to v1.3

--- port/stm32-wb55xx-nucleo/port/btstack_port.c
+++ port/stm32-wb55xx-nucleo/port/btstack_port.c
@@ -327,6 +327,7 @@ static void transport_deliver_hci_packets(void){
     // process hci packets
     while (xQueueReceive(hciEvtQueue, &hcievt, 0) == pdTRUE)
     {
+        log_error(" Event buffer address : %d", hcievt);
         switch (hcievt->evtserial.type)
         {
             case HCI_EVENT_PACKET:
